5ad7363a32d705f395f2f4152fa0e6b92c9cc5a7
sunseo18 <sunseo1818@gmail.com>
2022-10-09 13:28:08 +0900

    add: 2주차 기본 과제

---
+++ seminar2/createServer.js
@@ -0,0 +1,13 @@
+//http 모듈 가져오기
+const http = require("http");
+
+const port = 3000;
+
+http
+  .createServer((req, res) => {
+    res.write("<h1>IN SOPT SERVER!</h1>");
+    res.end("<p>awesome</p>");
+  })
+  .listen(port, () => {
+    console.log(`${port} 번 포트에서 대기중 !`);
+  });
