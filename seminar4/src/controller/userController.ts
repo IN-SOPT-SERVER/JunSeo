b85dd62501bd87a7fdc055d7c4102da435663896
sunseo18 <sunseo1818@gmail.com>
2022-11-05 16:50:04 +0900

    feat: user 생성

--- seminar4/src/controller/userController.ts
+++ seminar4/src/controller/userController.ts
@@ -1,6 +1,21 @@
 import { Request, Response } from "express";
 import { userService } from "../service";
 
+//~ 유저 생성
+const createUser = async (req: Request, res: Response) => {
+  //? request의 json 필드 명과 변수 명이 같으면
+  //? const { name, email, age } = req.body
+  //? 이렇게 코드를 작성할 수 있음  => 비구조화할당
+
+  const { userName, email, age } = req.body;
+  const data = await userService.createUser(userName, email, age);
+
+  if (!data) {
+    return res.status(404).json({ status: 404, message: "user create fail" });
+  }
+  return res.status(200).json({ status: 200, message: "user create success", data });
+};
+
 const getUserById = async (req: Request, res: Response) => {
   const { userId } = req.params;
 
@@ -11,9 +26,6 @@ const getUserById = async (req: Request, res: Response) => {
   }
   return res.status(200).json({ status: 200, message: "유저 조회 성공", data });
 };
-
-//~ 유저 생성
-const createUser = async (req: Request, res: Response) => {};
 
 //~ 유저 전체 조회
 const getAllUser = async (req: Request, res: Response) => {};
--- seminar4/src/service/userService.ts
+++ seminar4/src/service/userService.ts
@@ -1,7 +1,22 @@
-const { PrismaClient } = require("@prisma/client");
+import { PrismaClient } from "@prisma/client";
 const prisma = new PrismaClient();
 
-//* userId로 유저 조회
+//~ 유저 생성
+const createUser = async (userName: String, email: String, age: number) => {
+  const data = await prisma.user.create({
+    data: {
+      userName,
+      email,
+      age,
+    },
+    //? 데이터 속에 userName: name 으로 설정하면 db의 userName 컬럼에 name 데이터 저장할 수 있음
+    //? 필드 명만 잘 맞추면 data 객체 안의 순서는 중요하지 않음
+  });
+
+  return data;
+};
+
+//~ userId로 유저 조회
 const getUserById = async (userId: number) => {
   const user = await prisma.user.findUnique({
     where: {
@@ -12,8 +27,21 @@ const getUserById = async (userId: number) => {
   return user;
 };
 
+//~ 유저 전체 조회
+const getAllUser = async () => {};
+
+//~ 유저 정보 업데이트
+const updateUser = async () => {};
+
+//~ 유저 삭제
+const deleteUser = async () => {};
+
 const userService = {
   getUserById,
+  getAllUser,
+  updateUser,
+  deleteUser,
+  createUser,
 };
 
 export default userService;
